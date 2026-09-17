ec80f3b1a5c304b3532181506c12004a5fe4d874
Sameer <sameermudit29@gmail.com>
2026-09-17 13:23:31 +0530

    prepare vercel

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000";
+// const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "http://localhost:3000" || import.meta.env.VITE_API_URL;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
